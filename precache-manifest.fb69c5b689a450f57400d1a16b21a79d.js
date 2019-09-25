a266760bc1e887e88cd3ae088f61a360af10769d
kevinwit <kevin.zou@honeywell.com>
2019-09-25 12:14:55 +0800

    Updates

---
+++ precache-manifest.fb69c5b689a450f57400d1a16b21a79d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ee27264384ec431e0f4d37261de00ad",
+    "url": "/myrobots/index.html"
+  },
+  {
+    "revision": "641bcf58fa1d53a56d25",
+    "url": "/myrobots/static/css/main.5c95d7f4.chunk.css"
+  },
+  {
+    "revision": "61f1d4d5c574f5f9a6b3",
+    "url": "/myrobots/static/js/2.59dbe32a.chunk.js"
+  },
+  {
+    "revision": "641bcf58fa1d53a56d25",
+    "url": "/myrobots/static/js/main.03fd07ba.chunk.js"
+  },
+  {
+    "revision": "5b7c0cf60dc7ecaa908c",
+    "url": "/myrobots/static/js/runtime-main.7dc85acd.js"
+  }
+]);
